1a6872311ad094f154b1345aea6e82d642e9333c
Diogo Silverio <dblsilverio@gmail.com>
2018-10-07 02:33:00 -0300

    Fix

--- src/main/java/com/example/demo/domain/MessageRepository.java
+++ src/main/java/com/example/demo/domain/MessageRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
-public interface MessageRepository extends JpaRepository<Message, String>{
+public interface MessageRepository extends JpaRepository<Message, Long>{
 
 	List<Message> findByReady();
 	
